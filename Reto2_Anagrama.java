220ac762daa74c21bdcba4f58e7c6ace7499aada
Jordi <estiradojordi9@gmail.com>
2026-04-09 21:45:05 +0200

    Cambios para debug

---
+++ Reto2_Anagrama.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Reto2_Anagrama {
+    public static void main(String[] args) {
+        /*
+         * Escribe una función que reciba dos palabras (String) y retorne
+         * verdadero o falso (Bool) según sean o no anagramas.
+         * - Un Anagrama consiste en formar una palabra reordenando TODAS
+         * las letras de otra palabra inicial.
+         * - NO hace falta comprobar que ambas palabras existan.
+         * - Dos palabras exactamente iguales no son anagrama.
+         */
+
+        Scanner teclado = new Scanner(System.in);
+
+        // LE PEDIMOS LAS DOS PALABRAS LAS CUALES DEBEMOS COMPROBAR SI SON ANAGRAMAS
+
+        System.out.println("Comprobemos si son anagrama tus palabras:");
+        System.out.println("Palabra 1:");
+        String palabra1 = teclado.next();
+        System.out.println("Palabra 2:");
+        String palabra2 = teclado.next();
+
+        System.out.println(esAnagrama( palabra1 , palabra2));
+        
+    }
+
+
+    public static boolean esAnagrama(String palabra1 , String palabra2){
+
+        return false;
+    }
+}
